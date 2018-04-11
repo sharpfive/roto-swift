4637ff0799d920320ffa001369c258a9ec572852
Jaim Zuber <jaim@sharpfivesoftware.com>
2018-04-10 20:31:02 -0500

    Failing test

--- Tests/LeagueRostersScrapeTests.swift
+++ Tests/LeagueRostersScrapeTests.swift
@@ -20,4 +20,24 @@ class LeagueRostersScrapeTests: XCTestCase {
         
         XCTAssertEqual(league.teams.count, 12)
     }
+    
+    func testWestCalhounExists() throws {
+        let filename = "/Users/jaim/code/roto-swift/data/2017-espn.roster.txt"
+        
+        let repository = ESPNLeagueRostersRepository()
+        let league = repository.getLeagueRosters(for: filename)
+        
+        XCTAssertTrue(league.teams.contains(where: {$0.name == "WEST CALHOUN FADEAWAY"}))
+    }
+    
+    func testWestCalhounHas23Players() throws {
+        let filename = "/Users/jaim/code/roto-swift/data/2017-espn.roster.txt"
+        
+        let repository = ESPNLeagueRostersRepository()
+        let league = repository.getLeagueRosters(for: filename)
+        
+        let westCalhoun = league.teams.first(where: {$0.name == "WEST CALHOUN FADEAWAY"})
+        
+        XCTAssertEqual(westCalhoun?.players.count, 23)
+    }
 }
