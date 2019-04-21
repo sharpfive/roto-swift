a4fe4d6dae0f631196ab964fab54ac8ece23441c
Jaim Zuber <jaim@sharpfivesoftware.com>
2019-04-20 19:40:07 -0500

    WIP begin FreeAgentFinder

---
+++ Sources/FreeAgentFinder/main.swift
@@ -0,0 +1,7 @@
+
+import Foundation
+import RotoSwift
+
+let hitterFilename = "/Users/jaim/Dropbox/roto/2019/Zips/2019-04-19/Zips-auctionvalues-batters.csv"
+
+let pitcherFilename = "/Users/jaim/Dropbox/roto/2019/Zips/2019-04-19/Zips-auctionvalues-pitchers.csv"
