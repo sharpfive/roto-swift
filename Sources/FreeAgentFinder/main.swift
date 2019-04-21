37c883bbe617730c15f923291f0a8006ba275f2e
Jaim Zuber <jaim@sharpfivesoftware.com>
2019-04-21 16:11:57 -0500

    Add in 2021 values

--- Sources/FreeAgentFinder/main.swift
+++ Sources/FreeAgentFinder/main.swift
@@ -7,6 +7,8 @@ import RotoSwift
 let hitterFilename = "/Users/jaim/Dropbox/roto/2019/Zips/2019-04-19/Zips-auctionvalues-batters.csv"
 
 let hitterFilename2020 = "/Users/jaim/Dropbox/roto/2019/Zips/2019-04-19/Zips-projections-2020-batters-auctionvalues.csv"
+let hitterFilename2021 = "/Users/jaim/Dropbox/roto/2019/Zips/2019-04-19/Zips-projections-2021-batters-auctionvalues.csv"
+
 
 
 let pitcherFilename = "/Users/jaim/Dropbox/roto/2019/Zips/2019-04-19/Zips-auctionvalues-pitchers.csv"
@@ -14,6 +16,8 @@ let rosterFilename = "/Users/jaim/Dropbox/roto/2019/rosters/ESPN-2019-04-20.txt"
 
 var hitterValues = buildPlayerAuctionValuesArray(hitterFilename: hitterFilename, pitcherFilename: nil)
 var hitterValuesIn2020 = buildPlayerAuctionValuesArray(hitterFilename: hitterFilename2020, pitcherFilename: nil, csvFormat: .rotoswift)
+var hitterValuesIn2021 = buildPlayerAuctionValuesArray(hitterFilename: hitterFilename2021, pitcherFilename: nil, csvFormat: .rotoswift)
+
 var pitcherValues = buildPlayerAuctionValuesArray(hitterFilename: nil, pitcherFilename: pitcherFilename)
 let league = buildLeague(with: rosterFilename)
 
@@ -34,17 +38,38 @@ totalPlayerNames.forEach { name in
     }
 }
 
+struct PlayerKeeperValue {
+    let name: String
+    let value: Double
+    let nextYearValue: Double
+    let followingYearValue: Double
+
+    var totalValue: Double {
+        return value + nextYearValue + followingYearValue
+    }
+}
 //print("totalPLayerNames: \(totalPlayerNames)")
 print("Here are the best available hitters")
-sortedHitterValues.prefix(upTo: 20).forEach { hitter in
-    let valueIn2020 = hitterValuesIn2020.first(where: { $0.name == hitter.name })
-    print("\(hitter) - 2020: \(valueIn2020)")
+
+let asdf: [PlayerKeeperValue] = sortedHitterValues.prefix(upTo:100).map { hitter in
+    let valueIn2020 = hitterValuesIn2020.first(where: { $0.name == hitter.name })?.auctionValue ?? 0.0
+    let valueIn2021 = hitterValuesIn2021.first(where: { $0.name == hitter.name })?.auctionValue ?? 0.0
+    return PlayerKeeperValue(name: hitter.name, value: hitter.auctionValue, nextYearValue: valueIn2020, followingYearValue: valueIn2021)
 }
 
-print("Here are the best available pitchers")
-sortedPitcherValues.prefix(upTo: 20).forEach {
-    print($0)
+asdf.sorted(by: { $0.totalValue > $1.totalValue }).forEach {
+    print("\($0) - total: \($0.totalValue)")
 }
+
+//sortedHitterValues.prefix(upTo: 20).forEach { hitter in
+//    let valueIn2020 = hitterValuesIn2020.first(where: { $0.name == hitter.name })
+//    print("\(hitter) - 2020: \(valueIn2020)")
+//}
+
+//print("Here are the best available pitchers")
+//sortedPitcherValues.prefix(upTo: 20).forEach {
+//    print($0)
+//}
 
 // Attempt to use Set functionality, this would be OK, but we need the
 //let totalPlayerNames = league.teams.flatMap { $0.players.map { $0.name} }
