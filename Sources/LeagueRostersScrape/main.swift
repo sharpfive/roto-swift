cedc666aa95d351c2818cd11f7e6c292ac9c01f7
Jaim Zuber <jaim@sharpfivesoftware.com>
2019-07-01 13:52:39 -0500

    Cleanup

--- Sources/LeagueRostersScrape/main.swift
+++ Sources/LeagueRostersScrape/main.swift
@@ -77,8 +77,6 @@ guard let rostersFilename = rostersFilename else {
     exit(0)
 }
 
-//let filename = "/Users/jaim/Dropbox/roto/projections/2019-05-29/ESPN-rosters.txt"
-
 // open file and read text
 let leagueRostersDataString = try! String(contentsOfFile: rostersFilename, encoding: String.Encoding.ascii)
 
@@ -87,10 +85,6 @@ let repository = ESPNLeagueRostersRepository2019()
 let leagueRosters = repository.getLeagueRosters(from: leagueRostersDataString)
 
 print("leagueRosters.teams.count: \(leagueRosters.teams.count)")
-
-//let hitterFilename = "/Users/jaim/Dropbox/roto/2019/Zips/2019-04-19/Zips-auctionvalues-batters.csv"
-//
-//let pitcherFilename = "/Users/jaim/Dropbox/roto/2019/Zips/2019-04-19/Zips-auctionvalues-pitchers.csv"
 
 let fangraphsRepository = FanGraphsAuctionRepository(hitterFilename: hitterFilename, pitcherFilename: pitcherFilename)
 let projectedValues = fangraphsRepository.getAuctionValues()
@@ -105,74 +99,3 @@ printValues(for: leagueRosters, auctionValues: projectedValues, top: 15)
 
 print("Top 10")
 printValues(for: leagueRosters, auctionValues: projectedValues, top: 10)
-
-func printValues2(for leagueRosters: League, auctionValues: [PlayerAuction], top: Int) {
-    //var teamValues = [(name: String, value: Double)]()
-    //var totalTeamValues = 0.0
-
-    let teamPlayerValues = leagueRosters.teams.map { team -> (String, [PlayerAuction]) in
-        let playerAuctionValues: [PlayerAuction] = team.players.compactMap { player in
-            if let auctionPlayer = auctionValues.first(where: { $0.name == player.name }) {
-                return auctionPlayer
-            } else {
-                print("\(player.name) not found")
-                return nil
-            }
-        }
-
-        return (team.name, playerAuctionValues)
-    }
-}
-
-//    leagueRosters.teams.forEach { team in
-//        var teamValue = 0.0
-//
-//    let teamPlayerValues = leagueRosters.teams.map { team -> (String, [PlayerAuction]) in
-//        let playerAuctionValues: [PlayerAuction] = team.players.compactMap { player in
-//            if let auctionPlayer = auctionValues.first(where: { $0.name == player.name }) {
-//                return auctionPlayer
-//            } else {
-//                print("\(player.name) not found")
-//                return nil
-//            }
-//        }
-//
-//        return (team.name, playerAuctionValues)
-//    }
-//
-//
-//
-////    leagueRosters.teams.forEach { team in
-////        var teamValue = 0.0
-////
-////        let players: [PlayerAuction] = team.players.compactMap { player in
-////            if let auctionPlayer = auctionValues.first(where: { $0.name == player.name }) {
-////                return auctionPlayer
-////            } else {
-////                print("\(player.name) not found")
-////                return nil
-////            }
-////        }
-////
-////        let playerPool = players.sorted(by: { $0.auctionValue > $1.auctionValue })
-////            //.filter({ $0.auctionValue > 0.0})
-////            .prefix(top)
-////
-////        teamValue = playerPool.map({ $0.auctionValue}).reduce(0,+)
-////
-////        teamValues.append((name: team.name, value: teamValue))
-////        totalTeamValues = totalTeamValues + teamValue
-////    }
-////
-////    let orderedTeamValues = teamValues.sorted(by: { $0.value > $1.value } )
-////
-////    orderedTeamValues.forEach {
-////        print("team: \($0.name): \($0.value)")
-////    }
-//}
-////
-////printValues2(for: leagueRosters, auctionValues: projectedValues, top: 30)
-////
-////leagueRosters.teams.map { $0.players.reduce(0, { (result, player) -> Double in
-////    return result + player.auctionValue
-////})}
