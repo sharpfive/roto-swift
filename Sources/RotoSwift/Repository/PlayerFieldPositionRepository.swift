7009c8ff38966212f835e88fad432238a01c09ec
Jaim Zuber <jaim@sharpfivesoftware.com>
2019-07-02 18:55:27 -0500

    WIP broke a test

--- Sources/RotoSwift/Repository/PlayerFieldPositionRepository.swift
+++ Sources/RotoSwift/Repository/PlayerFieldPositionRepository.swift
@@ -8,49 +8,50 @@
 import Foundation
 import CSV
 
-enum PlayerCSVError: Error {
-    case missingField(fieldName: String)
-}
+public class PlayerFieldPositionRepository {
 
-public func getPlayers(from filename: String) throws -> [League.Player] {
-
-
-
-    let nameFieldValue = "ï»¿\"PlayerName\""
-    let elligiblePositionsFieldValue = "POS"
-    let playerDataCSV = try! String(contentsOfFile: filename, encoding: String.Encoding.ascii)
-
-    let csv = try! CSVReader(string: playerDataCSV,
-                             hasHeaderRow: true) // It must be true.
-
-    let headerRow = csv.headerRow!
-    let nameRowOptional = headerRow.index(of: nameFieldValue)
-
-    guard let nameRow = nameRowOptional else {
-        throw PlayerCSVError.missingField(fieldName: nameFieldValue)
+    enum PlayerCSVError: Error {
+        case missingField(fieldName: String)
     }
 
-    let elligiblePositionsFieldValueOptional = headerRow.index(of: elligiblePositionsFieldValue)
+    public func getPlayers(from filename: String) throws -> [League.Player] {
+        let nameFieldValue = "ï»¿\"PlayerName\""
+        let elligiblePositionsFieldValue = "POS"
+        let playerDataCSV = try! String(contentsOfFile: filename, encoding: String.Encoding.ascii)
 
-    guard let elligiblePositionsRow = elligiblePositionsFieldValueOptional else {
-        throw PlayerCSVError.missingField(fieldName: elligiblePositionsFieldValue)
-    }
+        let csv = try! CSVReader(string: playerDataCSV,
+                                 hasHeaderRow: true) // It must be true.
 
-    var players = [League.Player]()
+        let headerRow = csv.headerRow!
+        let nameRowOptional = headerRow.index(of: nameFieldValue)
 
-    while let row = csv.next() {
-        let nameString = row[nameRow]
-        let elligiblePositionsString = row[elligiblePositionsRow]
-
-        guard let eligblePositions = extractPositions(from: elligiblePositionsString) else {
-            continue
+        guard let nameRow = nameRowOptional else {
+            throw PlayerCSVError.missingField(fieldName: nameFieldValue)
         }
 
-        players.append(League.Player(name: nameString, eligiblePositions: eligblePositions))
+        let elligiblePositionsFieldValueOptional = headerRow.index(of: elligiblePositionsFieldValue)
 
-        
+        guard let elligiblePositionsRow = elligiblePositionsFieldValueOptional else {
+            throw PlayerCSVError.missingField(fieldName: elligiblePositionsFieldValue)
+        }
+
+        var players = [League.Player]()
+
+        while let row = csv.next() {
+            let nameString = row[nameRow]
+            let elligiblePositionsString = row[elligiblePositionsRow]
+
+            guard let eligblePositions = extractPositions(from: elligiblePositionsString) else {
+                print("no positions for \(nameString)")
+                continue
+            }
+
+            players.append(League.Player(name: nameString, eligiblePositions: eligblePositions))
+
+
+        }
+
+
+        return players
     }
-
-
-    return players
 }
