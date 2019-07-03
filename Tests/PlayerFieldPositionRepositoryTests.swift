1f2abb79e84131923f4d12db5551f2b59a88bacd
Jaim Zuber <jaim@sharpfivesoftware.com>
2019-07-02 19:05:48 -0500

    More tests

--- Tests/PlayerFieldPositionRepositoryTests.swift
+++ Tests/PlayerFieldPositionRepositoryTests.swift
@@ -33,4 +33,32 @@ class PlayerFieldPositionRepositoryTests: XCTestCase {
         // one player is PR/PH, we don't need to handle that case
         XCTAssertEqual(players.count, 499)
     }
+
+    func testYandyDiazExists() {
+        XCTAssertNotNil(players.contains(where: { $0.name == "Yandy Diaz" }))
+    }
+
+    func testYandyDiazPlaysDH() throws {
+        guard let yandy = players.first(where: { $0.name == "Yandy Diaz" }) else {
+            throw TestError.unexpectedNilError
+        }
+
+        XCTAssertTrue(yandy.eligiblePositions.contains{ $0 == League.FieldPosition.designatedHitter })
+    }
+
+    func testYandyDiazPlaysFirstBase() throws {
+        guard let yandy = players.first(where: { $0.name == "Yandy Diaz" }) else {
+            throw TestError.unexpectedNilError
+        }
+
+        XCTAssertTrue(yandy.eligiblePositions.contains{ $0 == League.FieldPosition.firstBase })
+    }
+
+    func testYandyDiazPlaysThirdBase() throws {
+        guard let yandy = players.first(where: { $0.name == "Yandy Diaz" }) else {
+            throw TestError.unexpectedNilError
+        }
+
+        XCTAssertTrue(yandy.eligiblePositions.contains{ $0 == League.FieldPosition.thirdBase })
+    }
 }
